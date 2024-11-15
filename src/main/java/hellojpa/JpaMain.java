fac21d6bfc8ffefea803a35f25fab4f2da23f649
Mo <jym2013@gmail.com>
2024-11-15 19:32:46 +0900

    양방향 연관관계1 : 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -13,33 +15,26 @@ public class JpaMain {
         tx.begin();
         try {
 
-            // Create : 입력시
-            // 연관관계 매핑 없는 방식으로!!!
-            //
-//            select * from member a
-//            join team t on a.team_id = t.team_id;
-
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member");
-//            member.setTeamId(team.getTeamId());
-            // 연관관계 매핑방식
             member.setTeam(team); //단방향 연관관계, 참조 저장
 
             em.persist(member);
+            em.flush();
+            em.clear();
 
-            // read 방식
-            // 하지만 쿼리안나감 : 영속성 컨텍스트 1차 캐시에서 들고옴
             Member findMember = em.find(Member.class, member.getId());
-            // 비워내기
-//            em.flush();
+            //멤버에서 팀으로, 팀에서 멤버로
+            List<Member> members = findMember.getTeam().getMembers();
 
-            // 참조를 사용해서 연관관계 조회
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getName() = " + findTeam.getName());
+            for (Member member1 : members) {
+                System.out.println("member1.getUsername() = " + member1.getUsername());
+            }
+
 
             tx.commit();
         } catch (Exception e) {
