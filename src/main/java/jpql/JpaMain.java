9bf66dbeffcee22c40dafaed80932703dc516ca4
Mo <jym2013@gmail.com>
2024-12-11 21:33:30 +0900

    페이징

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,28 +15,26 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
-
-            List<MemberDTO> resultList =
-                    em.createQuery("select new jpql.MemberDTO(m.username, m.age)" +
-                            "from Member m", MemberDTO.class)
-                            .getResultList();
-
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
-
+            for (int i = 0 ; i < 100 ; i++){
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
             em.flush();
             em.clear();
 
-//            List<Member> result = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
-//            Member findMember = result.get(0);
-//            findMember.setAge(20);
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Member member1 : resultList) {
+                System.out.println("member = " + member1);
+            }
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
